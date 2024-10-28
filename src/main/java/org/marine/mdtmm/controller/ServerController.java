96734f1208cfc33264f58d961ca5c4611311d918
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-10-28 16:55:33 +0630

    조직 선박

--- src/main/java/org/marine/mdtmm/controller/ServerController.java
+++ src/main/java/org/marine/mdtmm/controller/ServerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ServerController {
 
-    @GetMapping("/server")
+    @GetMapping("")
     public String server() {
         return "server";
     }
